2c27f568954962208232ac98ad3bfeb955aee21a
Vann-Dev <stevanvincent05@gmail.com>
2024-05-04 05:34:16 +0700

    refactor: change user and attendance to non unique

---
+++ migrations/20240503223334_refactor_change_user_and_attendance_to_non_unique/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX `Attendance_ip_address_key` ON `Attendance`;
+
+-- DropIndex
+DROP INDEX `Attendance_visitor_identifier_key` ON `Attendance`;
+
+-- DropIndex
+DROP INDEX `User_nim_key` ON `User`;
