15fffaaf4f972267db78f56f5e23e6959b5625ff
Vann-Dev <stevanvincent05@gmail.com>
2024-05-04 05:21:17 +0700

    feat: add get user route

--- src/routes/user/__init__.py
+++ src/routes/user/__init__.py
@@ -5,8 +5,8 @@ from .post import post_user
 user_bp = Blueprint("user", __name__, url_prefix="/user")
 
 @user_bp.route("", methods=["GET"])
-def get_user_route():
-    return get_user()
+async def get_user_route():
+    return await get_user()
 
 @user_bp.route("", methods=["POST"])
 async def post_user_route():
--- src/routes/user/get.py
+++ src/routes/user/get.py
@@ -1,2 +1,40 @@
-def get_user():
-    return "Hello, World!"
+from prisma import Prisma
+from flask import jsonify, request
+import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def get_user():
+    decoded = jwt.decode(
+        jwt=request.cookies.get("session"),
+        key=os.getenv("JWT_SECRET"),
+        algorithms=["HS256"]
+    )
+
+    db = Prisma()
+
+    await db.connect()
+        
+    data = await db.user.find_many(
+        where={
+            "account_id": decoded.get("id")
+        }
+    )
+
+    users = []
+
+    for item in data:
+        users.append({
+            "nim": item.nim,
+            "name": item.name,
+            "photo": item.photo,
+            "created_at": item.created_at,
+        })
+
+    await db.disconnect()
+    
+    return jsonify({
+        "data": users,
+    }), 200
