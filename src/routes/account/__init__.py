73d0cfb330c25f80f26e71486d00f444d4b8a3ab
Vann-Dev <stevanvincent05@gmail.com>
2024-05-03 23:50:38 +0700

    feat: add delete route

--- src/routes/account/__init__.py
+++ src/routes/account/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from .post import post_account
 from .get import get_account
+from .delete import delete_account
 
 account_bp = Blueprint("account", __name__, url_prefix="/account")
 
@@ -11,3 +12,7 @@ async def get_account_route():
 @account_bp.route("", methods=["POST"])
 async def post_account_route():
     return await post_account()
+
+@account_bp.route("/<id>", methods=["DELETE"])
+async def delete_account_route(id):
+    return await delete_account(id)
---
+++ src/routes/account/delete.py
@@ -0,0 +1,22 @@
+from flask import jsonify, request
+from prisma import Prisma
+
+async def delete_account(id):
+    db = Prisma()
+
+    await db.connect()
+        
+    data = await db.account.delete(
+        where={
+            "id": id
+        }
+    )
+    
+    await db.disconnect()
+
+    if not data:
+        return jsonify({"error": "Account not found"}), 404
+    
+    return jsonify({
+        "data": "Successfully deleted account",
+    }), 200
