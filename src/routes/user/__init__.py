034cb4bcef7d1bb0f51efd6a6265dc9bbedb3d5b
Vann-Dev <stevanvincent05@gmail.com>
2024-05-04 05:08:13 +0700

    feat: add post user

--- src/routes/user/__init__.py
+++ src/routes/user/__init__.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 from .get import get_user
+from .post import post_user
 
 user_bp = Blueprint("user", __name__, url_prefix="/user")
 
 @user_bp.route("", methods=["GET"])
 def get_user_route():
     return get_user()
+
+@user_bp.route("", methods=["POST"])
+async def post_user_route():
+    return await post_user()
---
+++ src/routes/user/post.py
@@ -0,0 +1,38 @@
+from flask import jsonify, request
+from prisma import Prisma
+import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def post_user():
+    if not request.json:
+        return jsonify({"error": "Request body is required"}), 400
+    
+    if not request.json.get("nim"):
+        return jsonify({"error": "NIM is required"}), 400
+    
+    if not request.json.get("name"):
+        return jsonify({"error": "Name is required"}), 400
+    
+    decoded = jwt.decode(
+        jwt=request.cookies.get("session"),
+        key=os.getenv("JWT_SECRET"),
+        algorithms=["HS256"]
+    )
+    
+    db = Prisma()
+    await db.connect()
+        
+    await db.user.create(
+        data={
+            "name": request.json.get("name"),
+            "nim": request.json.get("nim"),
+            "account_id": decoded.get("id")
+        }
+    )
+
+    await db.disconnect()
+    
+    return jsonify({"message": "User created successfully"}), 201
