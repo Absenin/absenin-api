a4754f7dcff5ad227cd3606899c94e234d99787c
Vann-Dev <stevanvincent05@gmail.com>
2024-05-05 02:19:20 +0700

    feat: change db schema

---
+++ migrations/20240504175339_refactor_remove_status_in_attendance/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `Attendance` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Attendance` DROP COLUMN `status`;
